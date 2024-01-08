3cbd1fa80849bafa71038e0cad9f90fedf72ff3b
IvanYankovSci993 <148336174+IvanYankovSci993@users.noreply.github.com>
2024-01-08 13:45:22 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ credential = ManagedIdentityCredential()
 secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
 
 # Access the secret values from Key Vault
-# database_secret = secret_client.get_secret("database-name-py")
+database_secret = secret_client.get_secret("database-name-py")
 # name_secret = secret_client.get_secret("Server-name-py")
 # password_secret = secret_client.get_secret("server-password-py")
 # username_secret = secret_client.get_secret("server-username-py")
 
 # Access the secret values
-# database_value = database_secret.value
+database_value = database_secret.value
 # name_value = name_secret.value
 # password_value = password_secret.value
 # username_value = username_secret.value
@@ -35,13 +35,13 @@ app = Flask(__name__)
 
 # database connection 
 # server = name_value
-# database = database_value
+database = database_value
 # username = username_value
 # password = password_value
 # driver = '{ODBC Driver 18 for SQL Server}'
 
 server = 'devops-project-server.database.windows.net'
-database = 'orders-db'
+# database = 'orders-db'
 username = 'maya'
 password = 'AiCore1237'
 driver = '{ODBC Driver 18 for SQL Server}'
